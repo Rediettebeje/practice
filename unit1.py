f20822cc10d13b66fb3b3101987335ec21c33a45
Rediettebeje <131832561+Rediettebeje@users.noreply.github.com>
2025-02-18 23:05:21 -0600

    created a functions that returns the first and last item from the list

--- unit1.py
+++ unit1.py
@@ -29,3 +29,65 @@ def product(a,b):
     return a * b
 
 print(product(22,7))
+
+# function classify_age() that takes an integer age as a parameter and returns "child" if the age is less
+#  than 18, and returns "adult"
+def classify_age(age):
+    if age < 18:
+        return "child"
+    else:
+        return "adult"
+
+print(classify_age(17))
+print(classify_age(19))
+
+# Write a function named what_time_is_it() that takes 
+# an integer hour as a parameter
+
+def what_time_is_it(hour):
+    if hour == 2:
+        return "taco time"
+    elif hour == 12:
+        return "peanut butter jelly time"
+    else:
+        return "nap time"
+
+print(what_time_is_it(2))
+print(what_time_is_it(7))
+print(what_time_is_it(12))
+
+
+def blackjack(score):
+    if score == 21:
+        print( "Blackjack")
+    elif score > 21:
+        print("Bust")
+    elif score >= 17 and score < 21:
+        print("Nice hand")
+    else:
+        print("Hit me")
+
+blackjack(24)
+blackjack(19)
+blackjack(21)
+blackjack(10)
+
+# a function get_first() that takes in a list as a parameter and 
+# returns the first item in the list. Return None if the list is empty.
+
+def get_first(list):
+    if len(list) == 0:
+        return "None"
+    return list[0]
+
+print(get_first([1,2,3,4]))
+
+# a function get_last() that takes in a list as a parameter and 
+# returns the last item in the list. Return None if the list is empty.
+
+def get_last(list):
+    if len(list) == 0:
+        return "None"
+    return list[len(list) - 1]
+
+print(get_last([1,2,3,4]))
