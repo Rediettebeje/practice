63394c62c8a13555fa12d347149498ff3b14fcd8
Rediettebeje <131832561+Rediettebeje@users.noreply.github.com>
2025-02-18 22:33:48 -0600

    Created new functions and called them in the script

---
+++ unit1.py
@@ -0,0 +1,31 @@
+# creating a function
+def hello_world():
+    print("helloworld!")
+
+hello_world() # calling
+
+
+def todays_mood():
+    mood = "😆"
+    print("Today's mood: " + mood)
+
+todays_mood()
+
+# problem3
+def print_menu(menu):
+    print("Lunch today is: " + menu)
+
+print_menu("🍕")
+
+# problem 4
+def sum(a, b):
+    return a + b
+
+sumof = sum(13,27)
+print(sumof * 2)
+
+# problem5
+def product(a,b):
+    return a * b
+
+print(product(22,7))
