b49f1ef220efbe1ecda0b29605f234eead010140
Rediettebeje <131832561+Rediettebeje@users.noreply.github.com>
2025-03-04 22:41:17 -0600

    created is_pangram function

--- unit3.py
+++ unit3.py
@@ -18,5 +18,30 @@ def swap_ends(my_str):
 
 print(swap_ends("hello"))
 
+# Write a function is_pangram() that takes in a string my_str as a parameter 
+# and returns True if the string is a pangram and False if not. 
+# A pangram is a sentence containing every letter in the English alphabet.
+
+def is_pangram(my_str):
+     # set() returns a set of unique characters in the string
+     result = set()
+     for char in my_str:
+          # isalpha() checks if the character is an alphabet
+          if char.isalpha():
+               result.add(char.lower())
+          if len(result) == 26:
+            return True
+     return False
+
+my_str = "The quick brown fox jumps over the lazy dog"
+print(is_pangram(my_str))
+
+str2 = "The dog jumped"
+print(is_pangram(str2))
+        
+     
+
+
+
 
 
