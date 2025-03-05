3ec56f762144b1e6471cd4c2a0074fbebcc22816
Rediettebeje <131832561+Rediettebeje@users.noreply.github.com>
2025-03-04 22:32:01 -0600

    practiced calling the function more than once

---
+++ unit3.py
@@ -0,0 +1,13 @@
+# Call the function so that it prints out the following to the 
+# console (without calling the function more than once):
+
+def count_mississippi(limit):
+    for num in range(1, limit):
+	    print( f"{num} mississippi")
+
+count_mississippi(6)
+
+
+
+
+
