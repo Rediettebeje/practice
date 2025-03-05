88b3884b9e501d7c29274a73716846dfbc89da98
Rediettebeje <131832561+Rediettebeje@users.noreply.github.com>
2025-03-04 22:35:09 -0600

    created swap function

--- unit3.py
+++ unit3.py
@@ -7,7 +7,16 @@ def count_mississippi(limit):
 
 count_mississippi(6)
 
+# Write a function swap_ends() that accepts a string my_str as a parameter and returns
+#  a new string where the first and last characters from my_str are swapped.
 
+def swap_ends(my_str):
+    # my_str[1:-1] is the string without the first and last characters
+    # my_str[-1] is the last character
+    # my_str[0] is the first charactergit
+    return my_str[-1] + my_str[1:-1] + my_str[0]
+
+print(swap_ends("hello"))
 
 
 
